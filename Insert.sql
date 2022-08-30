eb438d67a0181ba75f14d1b0cf49a5b7174f6343
αℓєχ __ <alex.ka.freitas@gmail.com>
2022-08-30 20:17:22 -0300

    Arquivo com os inserts feitos direto no sql

--- Insert.sql
+++ Insert.sql
@@ -3,3 +3,13 @@ select * from tb_postagens tp ;
 insert into tb_postagens (data,texto,titulo)
 values (now(),'Hoje começamos a desenvolver o blog pessoal','Primeiro Post');
 
+
+update tb_postagens set id = 3 where id = 1;
+
+INSERT INTO tb_postagens
+VALUES (1,current_timestamp(), 'Texto da postagem 01', 'Postagem 01');
+INSERT INTO tb_postagens 
+VALUES (2,current_timestamp(), 'Texto da postagem 02', 'Postagem 02');
+
+
+
